5b06c43ebaacef8cf0795ba9c2de360c4ac19846
Gabriel Dantas <gabrieldantaslp@gmail.com>
2025-04-03 07:33:32 -0300

    Unit testing the validation class with Class Validator

--- src/shared/domain/validators/class-validator-fields.ts
+++ src/shared/domain/validators/class-validator-fields.ts
@@ -4,8 +4,8 @@ import { FieldsErrors, IValidatorFields } from './validator-fields.interface'
 export abstract class ClassValidatorFields<PropsValidated>
   implements IValidatorFields<PropsValidated>
 {
-  errors: FieldsErrors
-  validatedData: PropsValidated
+  errors: FieldsErrors = null
+  validatedData: PropsValidated | null = null
   validate(data: any): boolean {
     const errors = validateSync(data)
     if (errors.length) {

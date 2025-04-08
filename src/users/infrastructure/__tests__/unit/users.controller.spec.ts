55e72421719e547cf26fa780a9c28fabc994630a
Gabriel Dantas <gabrieldantaslp@gmail.com>
2025-04-08 07:40:21 -0300

    Unit tests of the create method of the user controller

---
+++ src/users/infrastructure/__tests__/unit/users.controller.spec.ts
@@ -0,0 +1,43 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { UsersController } from '../../users.controller'
+import { UserOutput } from '@/users/application/dtos/user-output.dto'
+import { SignupUseCase } from '@/users/application/usecases/signup.usecase'
+import { SignUpDto } from '../../dto/signup.dto'
+
+describe('UsersController unit tests', () => {
+  let sut: UsersController
+  let id: string
+  let props: UserOutput
+
+  beforeEach(async () => {
+    sut = new UsersController()
+    id = '8ca4a0d7-f05d-4844-96f7-124ab42a9305'
+    props = {
+      id,
+      name: 'Jhon Doe',
+      email: 'a@a.com',
+      password: '1234',
+      createdAt: new Date(),
+    }
+  })
+
+  it('should be defined', () => {
+    expect(sut).toBeDefined()
+  })
+
+  it('should create a user', async () => {
+    const output: SignupUseCase.Output = props
+    const mockSignupUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(output)),
+    }
+    sut['signupUseCase'] = mockSignupUseCase as any
+    const input: SignUpDto = {
+      name: 'Jhon Doe',
+      email: 'a@a.com',
+      password: '1234',
+    }
+    const result = await sut.create(input)
+    expect(output).toMatchObject(result)
+    expect(mockSignupUseCase.execute).toHaveBeenCalledWith(input)
+  })
+})
